7b31d2b493e9bcade5393220c4f72eab64196db6
Rodrigo Verissimo <rodrigo.verissimo@ignicaodigital.com.br>
2022-06-08 19:57:29 +0000

    refactor: types

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,5 @@
 import IQueueProvider from './IQueueProvider';
-import QueueProvider from './implementations/sqs/QueueSqsProvider';
+import QueueSqsProvider from './implementations/sqs/QueueSqsProvider';
 import QueueInMemoryProvider from './implementations/queueInMemory/QueueInMemoryProvider';
 
-export default QueueProvider;
-export {
-  IQueueProvider,
-  QueueProvider,
-  QueueProvider as QueueSqsProvider,
-  QueueInMemoryProvider,
-};
+export { IQueueProvider, QueueSqsProvider, QueueInMemoryProvider };
